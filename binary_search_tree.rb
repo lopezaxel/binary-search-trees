820ec66549f90ab301a7403cc5632a534f2a6aed
Axel Lopez <lopezaxel@protonmail.com>
2020-07-08 12:16:40 -0300

    Add remove, find_node and find_node_parent methods

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -21,9 +21,19 @@ class Tree
     @root = build_tree(array)
   end
 
+  def build_tree(list) 
+    root = Node.new(list[0])
+
+    list[1..-1].each do |num|
+      add_node(list, root, Node.new(num)) 
+    end
+
+    root
+  end
+
   def add_node(list, root, node) 
-    return root if node.data == root.data
-    puts "root #{root.data} node #{node.data}" 
+    return root if node == root
+    # puts "root #{root.data} node #{node.data}" 
     
     if node >= root
       if root.right.nil?
@@ -46,19 +56,34 @@ class Tree
     add_node(self.array, self.root, Node.new(node))
   end
 
-  def build_tree(list) 
-    root = Node.new(list[0])
-
-    list[1..-1].each do |num|
-      add_node(list, root, Node.new(num)) 
+  def remove(node)
+    parent_node = find_parent_node(self.root, node)
+    target_node = find_node(parent_node, node)
+    
+    p parent_node, target_node
+  end
+  
+  def find_node(root, node)
+    if root.right.data == node
+      root.right
+    else
+      root.left
     end
+  end
 
-    root
+  def find_parent_node(root, node)
+     if node == root.data
+       return root
+     elsif node > root.data
+       return root if node == root.right.data
+       find_node(root.right, node) 
+     else
+       return root if node == root.left.data
+       find_node(root.left, node)
+     end
   end
 end
-
 a = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 9, 67, 6345, 324])
-p a.root
+# p a.root
+a.remove(3)
 
-a.insert(999)
-p a.root
