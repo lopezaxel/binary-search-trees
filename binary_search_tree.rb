775bafacfddae4e8083b3d86dcc38aa381bba2e4
Axel Lopez <lopezaxel@protonmail.com>
2020-07-08 10:40:09 -0300

    Add build_tree method create balanced binary tree

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,11 +1,11 @@
+require 'pry'
+
 class Node
   attr_accessor :data, :left, :right
   include Comparable
 
-  def initialize(data, left, right)
+  def initialize(data)
     @data = data
-    @left = left
-    @right = right
   end
 
   def <=>(node)
@@ -13,5 +13,46 @@ class Node
   end
 end
 
-a = Node.new(55, 10, 11)
-b = Node.new(10, 12, 13)
+class Tree
+  attr_accessor :array, :root
+
+  def initialize(array)
+    @array = array
+    @root = build_tree(array)
+  end
+
+  def check_nodes(list, root, node) 
+    return root if node.data == root.data
+    puts "root #{root.data} node #{node.data}" 
+    
+    if node >= root
+      if root.right.nil?
+        root.right = node
+      else
+        check_nodes(list, root.right, node) 
+      end
+    else
+      if root.left.nil?
+        root.left = node
+      else
+        check_nodes(list, root.left, node)
+      end
+    end
+    
+    root
+  end
+
+  def build_tree(list) 
+    root = Node.new(list[0])
+
+    list[1..-1].each do |num|
+      check_nodes(list, root, Node.new(num)) 
+    end
+
+    root
+  end
+end
+
+a = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 9, 67, 6345, 324])
+p a.root
+
